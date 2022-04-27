a7282b31d22ed9a11a052e7809ebab9e22dcbffb
Elaine Dong <elaine13@email.unc.edu>
2022-04-26 23:57:28 -0400

    added database

--- src/services/database.js
+++ src/services/database.js
@@ -1,1 +1,25 @@
 // Put your database code here
+
+"use strict"
+const Database = require('better-sqlite3')
+
+const db = new Database('log.db')
+
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
+let row = stmt.get()
+
+// check if table exists in database
+if (row === undefined) {
+    console.log('Your database appears to be empty. I will initialize it now.')
+    
+    const sqlInit = `
+        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time TEXT, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, status TEXT, referer TEXT, useragent TEXT);
+    `
+    
+    db.exec(sqlInit)
+    console.log('Your database has been initialized with a new table.')
+} else {
+    console.log('Database exists.')
+}
+
+module.exports = db
