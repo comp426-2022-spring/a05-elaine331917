079fb5f193e6961355c52ed4ae6274a1334afdb3
Elaine Dong <elaine13@email.unc.edu>
2022-04-27 00:38:09 -0400

    coin routes

--- src/routes/coin.js
+++ src/routes/coin.js
@@ -1,19 +1,28 @@
-app.get('/app/flip', (req, res) => {
+const express = require('express')
+const coin = require('../controllers/mycontrollers.js')
+const flip = express.Router();
+
+// one flip
+flip.route('/app/flip').get(function (req, res, next) {
     res.status(200).json({ 'flip': coin.coinFlip() })
 })
 
-app.get('/app/flips/:number', (req, res) => {
+// multiple flips
+flip.route('/app/flips/:number').get(function (req, res, next) {
     const flips = coin.coinFlips(req.params.number)
     const sum = coin.countFlips(flips)
     res.status(200).json({ 'raw': flips, 'summary': sum })
 })
 
-app.get('/app/flip/call/heads', (req, res) => {
+// match flip
+flip.route('/app/flip/call/heads').get(function (req, res, next) {
     const guess = coin.flipACoin('heads')
     res.status(200).json({ 'call': guess.call, 'flip': guess.flip, 'result': guess.result })
 })
 
-app.get('/app/flip/call/tails', (req, res) => {
+flip.route('/app/flip/call/tails').get(function (req, res, next) {
     const guess = coin.flipACoin('tails')
     res.status(200).json({ 'call': guess.call, 'flip': guess.flip, 'result': guess.result })
 })
+
+module.exports = flip
